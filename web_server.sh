2e8c0a1c2034f6ae8deca9b51a820563c7ab1dfa
bfourgeaud <benjamin.fourgeaud@gmail.com>
2020-04-07 20:07:10 +0700

    Wordpress edit wp-config.php

--- web_server.sh
+++ web_server.sh
@@ -203,16 +203,50 @@ install_Wordpress(){
 	rm -Rf $_TEMP_PATH/wordpress/
 	
 	echo "---> Setting up wp-config.php"
-	cp $_FILE_PATH/wp-config-sample.php $_FILE_PATH/wp-config.php
+	mv $_FILE_PATH/wp-config-sample.php $_FILE_PATH/wp-config.php
 	
-	local search_DBname="database_name_here"
-	local search_Username="username_here"
-	local search_Password="password_here"
+	#GET SALTS
+	WPSalts=$(wget https://api.wordpress.org/secret-key/1.1/salt/ -q -O -)
+	TablePrefx=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 9 | head -n 1)_
 	
-	sed -i "s/${search_DBname}/${_DB_NAME}/g" $_FILE_PATH/wp-config.php
-	sed -i "s/${search_Username}/${_USERNAME}/g" $_FILE_PATH/wp-config.php
-	sed -i "s/${search_Password}/${_PASSWORD}/g" $_FILE_PATH/wp-config.php
-	
+	cat <<EOF > wordpress/wp-config.php
+<?php
+/***Managed by JEEBIE.ME - Benjamin Fourgeaud***/
+
+define('DB_NAME', '$_DB_NAME');
+define('DB_USER', '$_USERNAME');
+define('DB_PASSWORD', '$_PASSWORD');
+define('DB_HOST', 'localhost');
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', '');
+
+/*WP Tweaks*/
+#define( 'WP_SITEURL', '' );
+#define( 'WP_HOME', '' );
+#define( 'ALTERNATE_WP_CRON', true );
+#define('DISABLE_WP_CRON', 'true');
+#define('WP_CRON_LOCK_TIMEOUT', 900);
+#define('AUTOSAVE_INTERVAL', 300);
+#define( 'WP_MEMORY_LIMIT', '256M' );
+#define( 'FS_CHMOD_DIR', ( 0755 & ~ umask() ) );
+#define( 'FS_CHMOD_FILE', ( 0644 & ~ umask() ) );
+#define( 'WP_ALLOW_REPAIR', true );
+#define( 'FORCE_SSL_ADMIN', true );
+#define( 'AUTOMATIC_UPDATER_DISABLED', true );
+#define( 'WP_AUTO_UPDATE_CORE', false );
+
+$WPSalts
+
+\$table_prefix = '$TablePrefx';
+
+define('WP_DEBUG', false);
+
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+require_once(ABSPATH . 'wp-settings.php');
+EOF
+
 	echo "---> Setting folder security"
 	chown -R $CURRENT_USER:$CURRENT_USER $_FILE_PATH
 	chmod -R 755 $_FILE_PATH
