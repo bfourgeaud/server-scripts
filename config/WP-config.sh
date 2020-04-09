6b4907d7712c4694b3fe55d70b2a173cd6a354c8
bfourgeaud <benjamin.fourgeaud@gmail.com>
2020-04-09 20:08:46 +0700

    Create WP Install Scripts

---
+++ config/WP-config.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+# Path to your custom themes/plugins
+PROJECT_PATH="/Users/$USER/Projects"
+
+# Path to your WordPress installs
+SITE_PATH="/Users/$USER/Sites"
+
+# Path to your custom Scripts folder
+SCRIPT_PATH="/Users/$USER/Scripts"
+
+# Base URL (sites will be setup as subdirectories)
+BASE_URL="http://localhost"
+
+# Database information
+DB_USER="root"
+DB_PASS="root"
+DB_HOST="localhost"
