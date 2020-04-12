88fab54c54ef20d573174d6f3a976c05ee12b0de
bfourgeaud <benjamin.fourgeaud@gmail.com>
2020-04-12 19:44:17 +0700

    sed problems

--- bin/wordpress.sh
+++ bin/wordpress.sh
@@ -46,7 +46,7 @@ then
   install_package "mariadb-server"
   install_package "mariadb-client"
 
-  DB_NAME= $(sed 's+.+_+g' <<< $DOMAIN)
+  DB_NAME= ${DOMAIN//./_}
 
   echo "---> Wordpress Database info (DBNAME: $DB_NAME) :"
 	read -p "Enter a username :" DB_USER
